713a0a924d78d4b6b2ea20775750598522bf09d9
Christian Hansen <447922@rts-365.dk>
2017-08-17 10:21:16 +0200

    Øvelse 1 i functionsopgaven er færdiggjort og forstået

--- js/oevelse_functions.js
+++ js/oevelse_functions.js
@@ -19,3 +19,52 @@ var sayHey = function sayHey(value) {
 }
 
 sayHey('test for hoisting'); // flyt ned under funktionen
+
+// Øvelse A
+printNavn("Gandi");
+printNavn("Hugo Jensen");
+printNavn("28/9-2000");
+
+function printNavn(who) {
+    console.log(who);
+}
+
+function udskriv(fornavn, efternavn, fodselsdag) {
+    console.log(fornavn, efternavn, fodselsdag);
+}
+udskriv('Gandi', 'Jensen', 2000);
+
+// Øvelse B
+printTal();
+
+function printTal() {
+    console.log(8 + 8);
+    console.log(8 - 8);
+    console.log(8 * 8);
+    console.log(8 / 8);
+    console.log(8 % 8);
+}
+
+function add(tal1, tal2) {
+    console.log(tal1 + tal2);
+    console.log(tal1 - tal2);
+    console.log(tal1 * tal2);
+    console.log(tal1 / tal2);
+    console.log(tal1 % tal2);
+}
+
+var result = 0;
+function add(tal) {
+    // result = result + tal;
+    result += tal;
+}
+
+function sub(tal) {
+    result -= tal;
+}
+
+add(5);
+add(8);
+sub(4);
+
+console.log(result);
